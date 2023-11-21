e6bb5493242b1cee8b5d329e53ef5572c068e082
Andrii Sharmar <130487443+shandrii83@users.noreply.github.com>
2023-11-21 15:42:33 +0100

    JS + LocalStorage

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+/* 
+const NOMBRE= "ANDRII";
+
+
+let saludo = 'Buenas tardes , estamos encantados de verte';
+const btn = document.querySelector(".btn");
+const muestra = document.getElementById("monstrar");
+
+
+const saludando = function saludando(){
+    muestra.innerHTML = saludo;
+}
+btn.onclick =saludando;
+ */
